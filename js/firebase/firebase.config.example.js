fb3dab3af9e0c2d6d8c7deee607946eb87065c3a
sato4app <sato.apps@quad.razor.jp>
2025-11-25 21:51:22 +0900

    remove firebase.config.example.js

--- js/firebase/firebase.config.example.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "your_api_key_here",
-  authDomain: "your_domain_here",
-  projectId: "your_project_id",
-  storageBucket: "your_bucket",
-  messagingSenderId: "your_sender_id",
-  appId: "your_app_id"
-};
