39a9d96074821c5851ae393f4d0bd9c3b8bd3295
sato4app <sato.apps@quad.razor.jp>
2025-11-25 15:48:04 +0900

    Move firebase.config.js to .gitignore and add example file

---
+++ js/firebase/firebase.config.example.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "your_api_key_here",
+  authDomain: "your_domain_here",
+  projectId: "your_project_id",
+  storageBucket: "your_bucket",
+  messagingSenderId: "your_sender_id",
+  appId: "your_app_id"
+};
